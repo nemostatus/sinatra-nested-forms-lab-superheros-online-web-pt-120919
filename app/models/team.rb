3fd6f17d73a7f2bf3a24a003f14451106fef97a9
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-03-24 14:44:44 +0000

    Done.

---
+++ app/models/team.rb
@@ -0,0 +1,15 @@
+class Team
+  attr_accessor :name, :power, :biography 
+  @@all = [] 
+  
+  def initialize(name,power,biography)
+  @name = name 
+  @power = power
+  @biography = biography
+  @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+end 
